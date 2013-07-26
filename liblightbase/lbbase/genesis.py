b393622b40b7f3c796f59d91591431bb5a1c601b
antony <antony>
2013-07-26 20:14:42 +0000

    Código de geração de bases completo
    git-svn-id: http://svn.brlight.net/svn/lightbase-neo/trunk/liblightbase@367 29b92fdf-8c97-4584-b987-84e8d3c556fa

--- liblightbase/lbbase/genesis.py
+++ liblightbase/lbbase/genesis.py
@@ -1,3 +1,7 @@
+
+from liblightbase.lbbase.fields import *
+from liblightbase.lbbase. __init__2 import *
+
 example = {
     'metadata':{
         'name':'base_name',
@@ -24,12 +28,12 @@ example = {
                 'Textual',
                 'Vazio'
             ]
-        }
+        },
         '2':{
-            'name': 'nome9',
-            'description': 'descricao9',
+            'name': 'nome2',
+            'description': 'descricao2',
             'multivalued': False,
-        }
+        },
         '3':{
             'name': 'nome3',
             'description': 'descricao3',
@@ -39,22 +43,22 @@ example = {
                 'Textual',
                 'Vazio'
             ]
-        }
+        },
         '5':{
-            'name': 'nome9',
-            'description': 'descricao9',
+            'name': 'nome5',
+            'description': 'descricao5',
             'multivalued': True,
-        }
+        },
         '6':{
             'name': 'nome6',
             'description': 'descricao6',
             'datatype': 'AlfaNumerico',
             'multivalued': True,
-            'indices':
+            'indices':[
                 'Textual',
                 'Vazio'
             ]
-        }
+        },
         '9':{
             'name': 'nome9',
             'description': 'descricao9',
@@ -64,7 +68,7 @@ example = {
                 'Textual',
                 'Vazio'
             ]
-        }
+        },
         '11':{
             'name': 'nome11',
             'description': 'descricao11',
@@ -77,3 +81,58 @@ example = {
         }
     },
 }
+
+
+def create_base(prelude):
+
+    """ Creates Base instance based on predefined object
+    """
+    metadata = prelude['metadata']
+    structure = prelude['structure']
+    context = prelude['context']
+
+    def parse_structure(_structure):
+
+        _content_list = list()
+        for node in _structure:
+
+            node_id = node['id']
+            node_context = context[str(node_id)]
+
+            # Do we have a group ? ...
+            if node.get('children'):
+                _content = parse_structure(node['children'])
+                group = Group(
+                    name = node_context['name'],
+                    description = node_context['description'],
+                    content = _content,
+                    multivalued = Multivalued(node_context['multivalued'])
+                )
+                _content_list.append(group)
+
+            # ... Or do we have a field ?
+            else:
+                field = Field(
+                    name = node_context['name'],
+                    description = node_context['description'],
+                    datatype = DataType(node_context['datatype']),
+                    indices = [Index(i) for i in node_context['indices']],
+                    multivalued = Multivalued(node_context['multivalued'])
+                )
+                _content_list.append(field)
+
+        return _content_list
+
+    content = parse_structure(structure)
+
+    base = Base(
+        name = metadata['name'],
+        description = metadata['description'],
+        content = content
+    )
+
+    return base
+
+
+base = create_base(example)
+print(base.json)
