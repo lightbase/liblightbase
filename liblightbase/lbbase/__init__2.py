b0948e849c03c27e3940d5bd9841a3c137b6b00d
antony <antony>
2013-07-29 14:52:12 +0000

    aleatory
    git-svn-id: http://svn.brlight.net/svn/lightbase-neo/trunk/liblightbase@370 29b92fdf-8c97-4584-b987-84e8d3c556fa

--- liblightbase/lbbase/__init__2.py
+++ liblightbase/lbbase/__init__2.py
@@ -1,6 +1,7 @@
 
 from liblightbase.lbbase import fields
 import json
+from voluptuous import Schema
 
 class Base():
     """
@@ -21,10 +22,10 @@ class Base():
         return self._content
 
     @content.setter
-    def content(self, g):
+    def content(self, c):
         content_list = list()
-        if type(g) is list:
-            for value in g:
+        if type(c) is list:
+            for value in c:
                 if isinstance(value, fields.Field) or isinstance(value, fields.Group):
                     content_list.append(value)
                 else:
@@ -32,38 +33,30 @@ class Base():
                     raise Exception(msg)
             self._content = content_list
         else:
-            msg = 'Type Error: content must be a list instead of %s' % g
+            msg = 'Type Error: content must be a list instead of %s' % c
             raise Exception(msg)
             self._content = None
 
     @property
     def object(self):
-
         """ Builds base object 
         """
-        _metadata = dict(
-            name = self.name,
-            description = self.description,
+        return dict(
+            content = [attr.object for attr in self.content],
+            metadata = dict(
+                name = self.name,
+                description = self.description,
+            )
         )
-
-        _content = list()
-        for attr in self.content:
-            _content.append(attr.object)
-
-        base_object= dict(
-            metadata = _metadata,
-            content = _content,
-        )
-
-        return base_object
 
     @property
     def schema(self):
-
-        """ Builds base schema
+        """ Builds base Schema
         """
-        return { attr.name: attr.schema for attr in self.content }
+        return Schema({attr.name: attr.schema for attr in self.content})
 
     @property
     def json(self):
+        """ Builds base JSON
+        """
         return json.dumps(self.object, ensure_ascii=True)
