a19b9d2f1a54b69dd626a9718b6d7c481194807c
antony <antony>
2013-09-03 16:51:12 +0000

    rest retrieving code
    git-svn-id: http://svn.brlight.net/svn/lightbase-neo/trunk/liblightbase@519 29b92fdf-8c97-4584-b987-84e8d3c556fa

--- liblightbase/lbbase/genesis.py
+++ liblightbase/lbbase/genesis.py
@@ -7,9 +7,8 @@ def json_to_base(base_json):
 
     """ Parses base_json and builds Base instance
     """
-    base_object = json.loads(base_json.encode('utf-8'))
-    base_metadata = base_object['metadata']
-    base_content = base_object['content']
+    base_metadata = base_json['metadata']
+    base_content = base_json['content']
 
     def assemble_content(content_object):
 
@@ -26,19 +25,12 @@ def json_to_base(base_json):
                 group_metadata = obj['group']['metadata']
                 group_content = obj['group']['content']
 
-                # Build multivalued instance
-                multivalued = group_metadata['multivalued']
-                _multivalued = Multivalued(multivalued)
-
-                # Build inner fields/groups
-                _content_list = assemble_content(group_content)
-
-                # Finally build group instance ...
+                # Build group instance ...
                 _group = Group(
                     name = group_metadata['name'],
                     description = group_metadata['description'],
-                    content = _content_list,
-                    multivalued = _multivalued
+                    multivalued = Multivalued(group_metadata['multivalued']),
+                    content = assemble_content(group_content),
                 )
 
                 # ... and append it to content list
@@ -48,10 +40,6 @@ def json_to_base(base_json):
             elif obj.get('field'):
                 field = obj['field']
 
-                # Build datatype instance
-                datatype = field['datatype']
-                _datatype = DataType(datatype)
-
                 # Build indices list
                 indices = field['indices']
                 _indices = list()
@@ -60,17 +48,14 @@ def json_to_base(base_json):
                         _index = Index(index)
                         _indices.append(_index)
 
-                # Build multivalued instance
-                multivalued = field['multivalued']
-                _multivalued = Multivalued(multivalued)
-
                 # Finally Build Field instance ...
                 _field = Field(
                     name = field['name'],
                     description = field['description'],
-                    datatype = _datatype,
+                    datatype = DataType(field['datatype']),
                     indices = _indices,
-                    multivalued = _multivalued
+                    multivalued = Multivalued(field['multivalued']),
+                    required = Required(field['required'])
                 )
 
                 # and append it to content list
@@ -84,6 +69,11 @@ def json_to_base(base_json):
     base = Base(
         name = base_metadata['name'],
         description = base_metadata['description'],
+        index_export = base_metadata['index_export'],
+        index_url = base_metadata['index_url'],
+        index_time = base_metadata['index_time'],
+        doc_extract = base_metadata['doc_extract'],
+        extract_time = base_metadata['extract_time'],
         content = _content
     )
 
