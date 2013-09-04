7a871b675d287806105978848cb8d6d085ba18b7
antony <antony>
2013-09-04 17:00:05 +0000

    adding standard lbtypes
    git-svn-id: http://svn.brlight.net/svn/lightbase-neo/trunk/liblightbase@529 29b92fdf-8c97-4584-b987-84e8d3c556fa

--- liblightbase/lbtypes/standard.py
+++ liblightbase/lbtypes/standard.py
@@ -1,28 +1,81 @@
 # -*- coding: utf-8 -*-
-from .base import lbtype
-from datetime import datetime
-from dateutil.parser import parse as date_parser
-    
-class String(lbtype):
-    """ lbtype string class
-    """
-    inner_type = str
-
-class Integer(lbtype):
-    """ lbtype integer class
-    """
-    inner_type = int
-
-class Float(lbtype):
-    """ lbtype float class
-    """
-    inner_type = float
-
-class DateTime(lbtype):
-    """ lbtype datetime class
-    """
-    inner_type = datetime
-    def __parse_string__(self, string):
-        self.value = date_parser(string)
-        return self.value
         
+class Standard():
+    def __repr__(self):
+        return "'%s'" % self.__class__.__name__
+
+class Texto(Standard):
+    def __call__(self, value):
+        return value
+
+class AlfaNumerico(Standard):
+    def __call__(self, value):
+        return value
+
+class Documento(Standard):
+    def __call__(self, value):
+        return value
+
+class Inteiro(Standard):
+    def __call__(self, value):
+        return value
+
+class Decimal(Standard):
+    def __call__(self, value):
+        return value
+
+class Moeda(Standard):
+    def __call__(self, value):
+        return value
+
+class AutoEnumerado(Standard):
+    def __call__(self, value):
+        return value
+
+class Data(Standard):
+    def __call__(self, value):
+        return value
+
+class Hora(Standard):
+    def __call__(self, value):
+        return value
+
+class DataHora(Standard):
+    def __call__(self, value):
+        return value
+
+class Imagem(Standard):
+    def __call__(self, value):
+        return value
+
+class Som(Standard):
+    def __call__(self, value):
+        return value
+
+class Video(Standard):
+    def __call__(self, value):
+        return value
+
+class URL(Standard):
+    def __call__(self, value):
+        return value
+
+class VerdadeiroFalso(Standard):
+    def __call__(self, value):
+        return value
+
+class Arquivo(Standard):
+    def __call__(self, value):
+        return value
+
+class HTML(Standard):
+    def __call__(self, value):
+        return value
+
+class Email(Standard):
+    def __call__(self, value):
+        return value
+
+class JSON(Standard):
+    def __call__(self, value):
+        return value
