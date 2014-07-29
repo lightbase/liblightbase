9e73e62b2173853a6934c358a67ccf8751165449
antonygc91 <antonygc91@gmail.com>
2014-07-29 15:43:25 -0300

    fix metaclass property serialization

--- liblightbase/lbutils/conv.py
+++ liblightbase/lbutils/conv.py
@@ -117,6 +117,8 @@ def document2dict(base, document, struct=None):
         try: value = getattr(document, sname)
         except AttributeError: pass
         else:
+            if isinstance(value, property):
+                continue
             _struct = base.get_struct(sname)
             if _struct.is_field:
                 dictobj[sname] = value
