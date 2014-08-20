22bad0748fd8bad127c24d8306fb656ee9974241
edulucio <eduardo.lucio@lightbase.com.br>
2014-08-20 15:54:06 -0300

    Mudança no módulo do ES

--- liblightbase/lbsearch/es.py
+++ liblightbase/lbsearch/es.py
@@ -5,8 +5,13 @@ import requests
 
 class ElasticSearch():
 	def search(self, urlNParameters, jsonRequest):
+
+		# dataTest = {"eventType": jsonRequestUTF8, "fields": "_metadata.id_doc"}
+		paramsTest = {"fields": "_metadata.id_doc"}
+
 		jsonRequestUTF8 = jsonRequest.encode(encoding='UTF-8',errors='strict')
 		response = requests.post(urlNParameters, 
+			params=paramsTest, 
 			data=jsonRequestUTF8, 
 			timeout=120)
 		return response.text
