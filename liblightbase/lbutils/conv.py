f54b58a2185fc28686047be9be21d141f9fd4d9d
edulucio <eduardo.lucio@lightbase.com.br>
2014-10-17 15:37:32 -0300

    Correções na recuperação dos dados dos objetos valorados dessa lib. Pois, ao recuperar uma classe representativa (objeto) de uma base ele traz atributos não setados que estavam gerando erros ao submeter o registro no nas ops create e update! By Questor

--- liblightbase/lbutils/conv.py
+++ liblightbase/lbutils/conv.py
@@ -127,13 +127,17 @@ def document2dict(base, document, struct=None):
     else:
         snames = struct.content.__snames__
     for sname in snames:
+        value = None
         try:
             value = getattr(document, sname)
         except AttributeError:
-            # Try to get dict key
-            value = document.get(sname)
-            if value is None:
-                continue
+            try:
+                value = document.get(sname)
+            except AttributeError:
+                pass
+            pass
+        if value is None:
+            continue
         _struct = base.get_struct(sname)
         if _struct.is_field:
             dictobj[sname] = value
