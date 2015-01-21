941e4c070d5f854a3fff180604183c23bcd68289
edulucio <eduardo.lucio@lightbase.com.br>
2015-01-21 12:01:31 -0200

    Ajuste na lib para uso do ES! By Questor

--- liblightbase/lbsearch/es.py
+++ liblightbase/lbsearch/es.py
@@ -8,6 +8,8 @@ import requests
 class ElasticSearch():
     def search(self, resourceURL, lbBaseInstance, jsonQuery, additionalParams={'lbquery': '1'}):
 
+        additionalParams = None;
+
         baseName = lbBaseInstance.metadata.name
         completeURL = resourceURL + "/" + baseName + "/es/_search"
 
@@ -25,9 +27,10 @@ class ElasticSearch():
 
 
         # Note: To debug! By Questor
-        # print("response.json()")
-
-        return Collection(lbBaseInstance, **response.json())
+        print(response.json())
+        # return Collection(lbBaseInstance, **response.json())
+        # return response
+        return response.json()
 
 # {'status': 500, 'type': 'Exception', 'error_message': 'SearchError: (ProgrammingError) syntax error at or near ")"\nLINE 4: WHERE id_doc in (25,)) AS anon_1\n                            ^\n \'SELECT count(*) AS count_1 \\nFROM (SELECT lb_doc_db_reg_anot_teses.id_doc AS lb_doc_db_reg_anot_teses_id_doc, lb_doc_db_reg_anot_teses.document AS lb_doc_db_reg_anot_teses_document \\nFROM lb_doc_db_reg_anot_teses \\nWHERE id_doc in (25,)) AS anon_1\' {}', 'request': {'path': '/api/db_reg_anot_teses/es/_search', 'client_addr': '192.168.25.254', 'user_agent': 'python-requests/2.3.0 CPython/3.2.2 Linux/2.6.32-431.20.3.el6.x86_64', 'method': 'POST'}}
 
